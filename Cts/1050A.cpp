f12473742db547af791dd4ec63425b6b0c4a2eea
Sameera Jape <sameerajape@gmail.com>
2025-09-13 22:24:43 +0530

    q added

---
+++ Cts/1050A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n,x;
+    cin>>t;
+    while(t--){
+        cin>>x>>n;
+        if(n%2==0) cout<<0<<"\n";
+        else cout<<x<<"\n";
+
+    }
+    return 0;
+}
